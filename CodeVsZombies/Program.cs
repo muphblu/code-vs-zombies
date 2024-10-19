91c9c5264f1d5560b4bc774d1e3e47c2d651b9e9
Aidar Shaikhiev <muphblu09@gmail.com>
2024-10-20 04:21:27 +0500

    more complex target

--- CodeVsZombies/Program.cs
+++ CodeVsZombies/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 
@@ -6,27 +7,54 @@ internal class Program
 {
     public static void Main(string[] args)
     {
+        const float zombieStep = 400;
+        const float playerStep = 1000;
+        const float shotRange = 2000;
+        
         IInputProvider inputProvider = args.FirstOrDefault() is var inputPath && inputPath is not null
                                            ? new FileInputProvider(inputPath)
                                            : new ConsoleInputProvider();
-                                   
+        var willBeEaten = new List<int>();
         var inputParser = new InputParser(inputProvider);
         while (true)
         {
             var turnInput = inputParser.ParseTurnInput();
+            willBeEaten = turnInput.Humans.Select(x=>x.Id).Intersect(willBeEaten).ToList();   
+            var prioritizedZombies = 
+                turnInput.Zombies
+                         .Select(
+                    z =>
+                    {
+                        var closestHuman = turnInput.Humans.Select(h => (Human: h, Distance: Vector2.Distance(h.Position, z.Position))).MinBy(x => x.Distance);
+                        if (willBeEaten.Contains(closestHuman.Human.Id))
+                            return (Zombie:z, closestHuman.Human, StepsZombieToHuman: float.MaxValue, StepsPlayerToHuman: float.MaxValue, PlayerIsTarget: false);
 
-            var prioritizedZombies = turnInput.Zombies.OrderBy(z => turnInput.Humans
-                                                    .Select(h => (Human: h,
-                                                                     Distance: Vector2.Distance(
-                                                                         h.Position,
-                                                                         z.Position)))
-                                                    .MinBy(x => x.Distance).Distance +
-                                           Vector2.Distance(z.Position, turnInput.PlayerPosition));
+                        var distanceZombieToPlayer = Vector2.Distance(z.Position, turnInput.PlayerPosition); 
+                        var stepsZombieToHuman = closestHuman.Distance / zombieStep;
+                        var distancePlayerToHuman = Vector2.Distance(closestHuman.Human.Position, turnInput.PlayerPosition);
+                        var stepsPlayerToHuman = Math.Max((distancePlayerToHuman - shotRange) / playerStep, 0);
+                        //human will be eaten
+                        if (stepsZombieToHuman < stepsPlayerToHuman)
+                        {
+                            willBeEaten.Add(closestHuman.Human.Id);
+                            return (Zombie: z, closestHuman.Human, StepsZombieToHuman: float.MaxValue, StepsPlayerToHuman: float.MaxValue, PlayerIsTarget: false);
+                        }
+
+                        var playerIsTarget = distanceZombieToPlayer < closestHuman.Distance;
+                        
+                        return
+                            (Zombie: z, closestHuman.Human, StepsZombieToHuman: stepsZombieToHuman,
+                                StepsPlayerToHuman: stepsPlayerToHuman, 
+                                PlayerIsTarget: playerIsTarget);
+                    })
+                         .OrderBy(x=> x.StepsPlayerToHuman + x.StepsZombieToHuman);
             
             var priorityTarget = prioritizedZombies.First();
-            var targetNextPosition = priorityTarget.NextPosition;
+            var targetNextPosition = priorityTarget.Zombie.NextPosition;
+            var zombieToHumanMidpoint = (priorityTarget.Zombie.NextPosition + priorityTarget.Human.Position) / 2;
+            var playerNextPosition = priorityTarget.PlayerIsTarget ? priorityTarget.Zombie.Position : zombieToHumanMidpoint;
 
-            Console.WriteLine($"{targetNextPosition.X} {targetNextPosition.Y}");
+            Console.WriteLine($"{Math.Ceiling(playerNextPosition.X)} {Math.Ceiling(playerNextPosition.Y)}");
         }
     }
 }
