99701fb311dd82385896b59fa65262ec781c8b9f
Aidar Shaikhiev <muphblu09@gmail.com>
2024-10-20 03:17:38 +0500

    Simple prioritize strategy
    choose zombie closest to a human + closest to palyer

--- CodeVsZombies/InputParse/InputParser.cs
+++ CodeVsZombies/InputParse/InputParser.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
 
 public class InputParser
@@ -11,12 +10,31 @@ public class InputParser
         _inputProvider = inputProvider;
     }
 
-    public TurnInput ParseMonthInput()
+    public TurnInput ParseTurnInput()
     {
-        return new TurnInput();
+        var parts = _inputProvider.ReadLine().Split(' ');
+        var playerPosition = new Vector2(int.Parse(parts[0]), int.Parse(parts[1]));
+        var humanCount = int.Parse(_inputProvider.ReadLine());
+        var humans = new List<Human>();
+        for (var i = 0; i < humanCount; i++)
+        {
+            parts = _inputProvider.ReadLine().Split(' ');
+            humans.Add(new Human(int.Parse(parts[0]), new Vector2(int.Parse(parts[1]), int.Parse(parts[2]))));
+        }
+        
+        var zombieCount = int.Parse(_inputProvider.ReadLine());
+        var zombies = new List<Zombie>();
+        for (var i = 0; i < zombieCount; i++)
+        {
+            parts = _inputProvider.ReadLine().Split(' ');
+            zombies.Add(new Zombie(int.Parse(parts[0]), new Vector2(int.Parse(parts[1]), int.Parse(parts[2])), new Vector2(int.Parse(parts[3]), int.Parse(parts[4]))));
+        }
+        
+        return new TurnInput(playerPosition, humans, zombies);
     }
 }
 
-public record TurnInput
-{
-}
+public record Human(int Id, Vector2 Position);
+public record Zombie(int Id, Vector2 Position, Vector2 NextPosition);
+
+public record TurnInput(Vector2 PlayerPosition, List<Human> Humans, List<Zombie> Zombies);
--- CodeVsZombies/Program.cs
+++ CodeVsZombies/Program.cs
@@ -1,8 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Linq;
+using System.Numerics;
 
-// If "inputPath" console arg was provided, read input from inputPath, else read from console
-// See launchsettings.json
-IInputProvider inputProvider = args.FirstOrDefault() is var inputPath && inputPath is not null
-                                   ? new FileInputProvider(inputPath)
-                                   : new ConsoleInputProvider();
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        IInputProvider inputProvider = args.FirstOrDefault() is var inputPath && inputPath is not null
+                                           ? new FileInputProvider(inputPath)
+                                           : new ConsoleInputProvider();
                                    
+        var inputParser = new InputParser(inputProvider);
+        while (true)
+        {
+            var turnInput = inputParser.ParseTurnInput();
+
+            var prioritizedZombies = turnInput.Zombies.OrderBy(z => turnInput.Humans
+                                                    .Select(h => (Human: h,
+                                                                     Distance: Vector2.Distance(
+                                                                         h.Position,
+                                                                         z.Position)))
+                                                    .MinBy(x => x.Distance).Distance +
+                                           Vector2.Distance(z.Position, turnInput.PlayerPosition));
+            
+            var priorityTarget = prioritizedZombies.First();
+            var targetNextPosition = priorityTarget.NextPosition;
+
+            Console.WriteLine($"{targetNextPosition.X} {targetNextPosition.Y}");
+        }
+    }
+}
